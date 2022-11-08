4d6e74c83ed1ed247b7676769addf7a6a203afa4
Dragnell Nguyễn <bang2001vl@outlook.com.vn>
2022-11-08 18:14:55 +0700

    Fix wrong namespace

--- Dietapp/Dietapp/MainWindow.xaml.cs
+++ Dietapp/Dietapp/MainWindow.xaml.cs
@@ -6,8 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using Dietapp.Database;
-using TodoApp1;
 
 namespace Dietapp
 {
---
+++ Dietapp/Dietapp/Windows/UserManagerWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dietapp.Windows
+{
+    /// <summary>
+    /// Interaction logic for UserManagerWindow.xaml
+    /// </summary>
+    public partial class UserManagerWindow : Window
+    {
+        public UserManagerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
